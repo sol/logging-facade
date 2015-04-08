0cf6d0a238e9f01c53d882c527c97c088bc3cb9a
Julian K. Arni <jkarni@gmail.com>
2015-04-08 11:43:55 +0200

    Add function to capture logs

--- test/System/Logging/FacadeSpec.hs
+++ test/System/Logging/FacadeSpec.hs
@@ -5,18 +5,34 @@ import           Data.IORef
 
 import           System.Logging.Facade.Types
 import           System.Logging.Facade.Sink
-import           System.Logging.Facade
+import           System.Logging.Facade as Log
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
+  let logToIORef :: IORef [LogRecord] -> LogSink
+      logToIORef ref record = modifyIORef ref (record :)
+
   describe "info" $ do
     it "writes a log message with log level INFO" $ do
       ref <- newIORef []
-      let captureLogMessage :: LogSink
-          captureLogMessage record = modifyIORef ref (record :)
-      setLogSink captureLogMessage
+      setLogSink $ logToIORef ref
       info "some log message"
       readIORef ref `shouldReturn` [LogRecord INFO Nothing "some log message"]
+
+  describe "captureLogs" $ do
+    it "returns all logs of an action" $ do
+      (logs, ()) <- captureLogs $ do
+        Log.trace "this should be captured"
+        Log.trace "this should be captured next"
+      logs `shouldBe` [ LogRecord TRACE Nothing "this should be captured"
+                      , LogRecord TRACE Nothing "this should be captured next"
+                      ]
+
+    it "prevents logs from going to the log sink" $ do
+      ref <- newIORef []
+      setLogSink $ logToIORef ref
+      _ <- captureLogs $ Log.trace "this should be captured"
+      readIORef ref `shouldReturn` []
